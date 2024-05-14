4eba3d01475f9e0e0e859aafa4934af95e9b1418
MLDODSON <paola@dominicansoul.com>
2024-05-14 11:51:54 -0500

    JS Basics
    Using helper methods.

---
+++ js tutorial/app.js
@@ -0,0 +1,9 @@
+// helper methods
+
+// document.write
+// alert
+// console.log
+
+document.write("I love cheesecake!");
+alert('The Martians are attacking!');
+console.log("Make my funk the P-Funk!");
