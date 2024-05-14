b9c321c6c395df95b075794549b1888639acff94
MLDODSON <paola@dominicansoul.com>
2024-05-14 12:35:37 -0500

    JS Basics - Challenge 1
    Javascript challenge 1: Variables.

---
+++ Challeges/app.js
@@ -0,0 +1,17 @@
+/*
+## Variables #1
+
+1. Create "firstName" and "last_name" variables.
+2. Assign your values.
+3. Create an "address" variable and assign it a value of "main street".
+4. Re-assign a value of "first street" to the address variable.
+5. Log all the values to the console.
+*/
+
+const firstName = "'Fat' Albert";
+const last_name = "Johnson";
+let address = "Main Street";
+address = "First Street";
+
+console.log(firstName, last_name);
+console.log(address);
