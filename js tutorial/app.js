d7b0e2944e5ab432542bc447d4c0b58af9bb31df
MLDODSON <paola@dominicansoul.com>
2024-05-14 12:17:08 -0500

    JS Basics
    Practice using Javascript variables.

--- js tutorial/app.js
+++ js tutorial/app.js
@@ -4,6 +4,39 @@
 // alert
 // console.log
 
+/*
 document.write("I love cheesecake!");
 alert('The Martians are attacking!');
 console.log("Make my funk the P-Funk!");
+*/
+
+// Variables
+
+let name = "Mike";
+console.log(name);
+
+let city = "Puerto Plata is a beautiful city.";
+console.log(city);
+
+let team = "Rams";
+// the variable team is reassigned
+team = "Chargers";
+console.log(team);
+
+const age = 56;
+// variables using const can't be reassigned. Trying to do so will cause an error
+// age = 32;
+console.log(age);
+
+let address, state, zip;
+
+address = "1313 Mockingbird Lane";
+state = "Feel Good, Missouri";
+zip = "63131";
+
+console.log(`My mailing address is ${address}, ${state}, ${zip}.`);
+
+const fullName = "Michael Dodson";
+const career = "Web Developer";
+
+console.log(`My name is ${fullName}. I am a ${age} year old ${career}.`);
