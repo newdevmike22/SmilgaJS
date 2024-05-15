0e991d6e902048741b76203a3b1c0058851bcbe5
MLDODSON <paola@dominicansoul.com>
2024-05-15 11:29:15 -0500

    JS Basics - Challenge 2
    JS Basics - Challenge 2: String Concatenation.

--- Challeges/app.js
+++ Challeges/app.js
@@ -6,7 +6,6 @@
 3. Create an "address" variable and assign it a value of "main street".
 4. Re-assign a value of "first street" to the address variable.
 5. Log all the values to the console.
-*/
 
 const firstName = "'Fat' Albert";
 const last_name = "Johnson";
@@ -15,3 +14,18 @@ address = "First Street";
 
 console.log(firstName, last_name);
 console.log(address);
+*/
+
+/*
+## String Concatenation #2
+
+1. Create "street" and "country" variables.
+2. Assign your values.
+3. Create a "fullMailingAddress" variable and assign it the values of "street + country".
+4. Log "fullMailingAddress" to the console.
+*/
+
+const street = "3226 Itaska Street, St. Louis, MO 63111";
+const country = "USA";
+const fullMailingAddress = `${street} • ${country}`;
+console.log(fullMailingAddress);
