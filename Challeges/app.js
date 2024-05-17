d57e07d4817ff9012ae7f96d9577c342f6db840c
MLDODSON <paola@dominicansoul.com>
2024-05-16 19:58:39 -0500

    JS Basics -Numbers
    JS Basics. Learning numbers and Challenge 3.

--- Challeges/app.js
+++ Challeges/app.js
@@ -14,18 +14,50 @@ address = "First Street";
 
 console.log(firstName, last_name);
 console.log(address);
-*/
 
-/*
+
 ## String Concatenation #2
 
 1. Create "street" and "country" variables.
 2. Assign your values.
 3. Create a "fullMailingAddress" variable and assign it the values of "street + country".
 4. Log "fullMailingAddress" to the console.
-*/
 
 const street = "3226 Itaska Street, St. Louis, MO 63111";
 const country = "USA";
 const fullMailingAddress = `${street} • ${country}`;
 console.log(fullMailingAddress);
+*/
+
+/*
+
+## Numbers #3
+
+1. Create "score1", "score2" and "score3" variables.
+2. Assign each values between 1-100.
+3. Calculate the total score and average score and assign them variables.
+4. Log "totalScore" and "averageScore" to the console.
+5. Create a "plates" variable with a value of 20.
+6. Create a "people" variable with a value of 7.
+7. Calculate the remaining plates using modulus and assign it to variable.
+8. Add 1 to the remaining plates. 
+9. Create a message variable and display 'There are (your value goes here) plates available.'
+10. Log the message.
+*/
+
+const score1 = 85;
+const score2 = 95;
+const score3 = 75;
+const totalScore = score1 + score2 + score3;
+const averageScore = totalScore / 3;
+
+console.log(totalScore);
+console.log(averageScore);
+
+const plates = 20;
+const people = 7;
+let remainingPlates = plates % people;
+remainingPlates +=1;
+const message = `There are ${remainingPlates} plates available.`;
+
+console.log(message);
--- js tutorial/app.js
+++ js tutorial/app.js
@@ -40,3 +40,35 @@ const fullName = "Michael Dodson";
 const career = "Web Developer";
 
 console.log(`My name is ${fullName}. I am a ${age} year old ${career}.`);
+
+// Numbers
+
+const addition = 35 + 100;
+const subtraction  = 120 - 60;
+const multi = 9 * 7;
+const division = 75 / 3;
+const decimal = 2.6;
+
+let number = 45;
+number +=5;
+
+let number2 = 50;
+number2 -=10;
+
+let number3 = 20;
+number3 *=3;
+
+let number4 = 60;
+number4 /=4;
+
+const candyPieces = 17;
+const children = 3;
+const amount = candyPieces % children;
+
+console.log(addition, subtraction, multi, division, decimal);
+console.log(addition - division);
+console.log(number);
+console.log(number2);
+console.log(number3);
+console.log(number4);
+console.log(amount);
